38ed32314f0a99202ad93967c3eff391defa9b50
AmanVatsSharma <aman95026@gmail.com>
2023-06-02 10:13:47 +0530

    feat: Add error boundary for graceful crashes 💥

---
+++ components/ErrorBoundary.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import { Component, ReactNode } from 'react';
+import { logger } from '@/lib/utils/logger';
+
+interface Props {
+  children: ReactNode;
+  fallback?: ReactNode;
+}
+
+interface State {
+  hasError: boolean;
+  error?: Error;
+}
+
+export class ErrorBoundary extends Component<Props, State> {
+  public state: State = {
+    hasError: false
+  };
+
+  public static getDerivedStateFromError(error: Error): State {
+    return { hasError: true, error };
+  }
+
+  public componentDidCatch(error: Error) {
+    logger.error('Error boundary caught error:', { error });
+  }
+
+  public render() {
+    if (this.state.hasError) {
+      return this.props.fallback || (
+        <div className="p-4 bg-red-50 border border-red-200 rounded-md">
+          <h2 className="text-red-800 font-semibold">Something went wrong</h2>
+          <p className="text-red-600 text-sm mt-1">
+            {this.state.error?.message || 'An unexpected error occurred'}
+          </p>
+        </div>
+      );
+    }
+
+    return this.props.children;
+  }
+} 
